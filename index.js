6e1bda3b384b14545b879a97e538bf06954e7393
mdshahrouq <mdshah1994@gmail.com>
2017-08-11 12:04:38 +0530

    Added profile

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/index.html');
@@ -55,7 +56,11 @@ function onConnect(socket){
 	// 	console.log('User Disconnect');
 	// });
 //});
+ 
+// http.listen(process.env.PORT || 3000, function(){
+//   console.log('listening on *:3000');
+// });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function() {
+    console.log('Our app is running on ' + port);
 });
